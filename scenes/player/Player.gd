a363259a08dd8619daec8cb31d5df97e97acb94c
Ali Almishri <ali.almishri@ucalgary.ca>
2025-03-16 11:34:23 -0600

    sliding mechanic

--- scenes/player/Player.gd
+++ scenes/player/Player.gd
@@ -7,6 +7,7 @@ const JUMP_VELOCITY = -400.0
 const DECEL_RATE = 15.0
 var direction : float
 enum {IDLE, RUN, JUMP, SKID, SLIDE}
+const SLIDING_SPEED = SPEED + 15
 
 var movement_state: int = IDLE
 
@@ -38,22 +39,28 @@ func _physics_process(delta):
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
+		
+	if Input.is_action_pressed("ui_down"):
+		#keep sliding as long as button is held
+		#momentum is locked here
+		#print("helloegstgh")
+		movement_state = SLIDE
+		if((%AnimatedSprite2D.flip_h)):
+			velocity.x = move_toward(velocity.x,SLIDING_SPEED*-1,DECEL_RATE)
+		else:
+			velocity.x = move_toward(velocity.x,SLIDING_SPEED, DECEL_RATE)
+		move_and_slide()
+		return
 
 	if is_on_floor():
-		if velocity.x != 0:
-			movement_state = RUN
-		else:
-			movement_state = IDLE
-			
 		# Get the input direction and handle the movement/deceleration.
-		# As good practice, you should replace UI actions with custom gameplay actions.
 		direction = Input.get_axis("ui_left", "ui_right")
-		if direction:#move in the directionsdadwsa
+		if direction: #move in the directionsdadwsa
 			if direction == 1:
 				%AnimatedSprite2D.flip_h = false
 			else:
 				%AnimatedSprite2D.flip_h = true
-			#movement_state = SKID
+			movement_state = RUN
 			velocity.x = SPEED * direction
 		else:#stop moving
 			if(velocity.x == 0):
@@ -61,7 +68,7 @@ func _physics_process(delta):
 			else:
 				movement_state = SKID
 				velocity.x = move_toward(velocity.x, 0, DECEL_RATE) #move toward 0 while holding LEFT
-		
+	
 	else:
 		movement_state = JUMP
 		# Add the gravity.
