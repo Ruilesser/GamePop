551fca5cdb4f32a229c12d7a2f20513c4414cabb
Alan Bach <kagamibach@gmail.com>
2025-03-15 23:28:47 -0600

    Play the animation only once

--- scenes/player/Animator.gd
+++ scenes/player/Animator.gd
@@ -2,12 +2,13 @@ extends AnimatedSprite2D
 
 const BASE_ANIMATION_SPEED: int = 2
 
+var old_movement_state: int = Enums.MovementState.IDLE
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
-
-func _process(_delta):
+func _update_animation():
 	match get_parent().get_meta("MovementState"):
 		Enums.MovementState.IDLE:
 			%AnimatedSprite2D.play("idle")
@@ -20,3 +21,8 @@ func _process(_delta):
 			%AnimatedSprite2D.play("idle")
 		Enums.MovementState.SLIDE:
 			%AnimatedSprite2D.play("slide")
+
+func _process(_delta):
+	if old_movement_state != get_parent().get_meta("MovementState"):
+		old_movement_state = get_parent().get_meta("MovementState")
+		_update_animation()
