2ee6eeb29ef000249eee59c7931f53fa16de86e5
Alan Bach <kagamibach@gmail.com>
2025-03-16 17:58:16 -0600

    Detect custom group

--- scenes/util/combat/Combat.gd
+++ scenes/util/combat/Combat.gd
@@ -70,12 +70,12 @@ func _get_attack_function(group: String, damage: int):
 
 
 # Process the attacking logic.
-func process_attacking(damage: int):
+func process_attacking(damage: int, group: String):
 	# Could use a FSM here but I don't want to overcomplicate things.
 	if %Stun.is_stunned():
 		return
 	var reset_stun = %Stun.set_attacking_stun()
-	var attack_function = _get_attack_function("enemy", damage)
+	var attack_function = _get_attack_function(group, damage)
 	# The entirety of this is cursed please ignore
 	%Hitbox.connect("body_entered", attack_function)
 	%AnimatedSprite2D.play("punch%d" % (combo_number + 1))
