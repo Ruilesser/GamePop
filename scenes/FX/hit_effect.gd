72adaeb819b7da6b96dfb3100b9901c0b049df35
Rui <157858530+Ruilesser@users.noreply.github.com>
2025-03-16 13:24:30 -0600

    Changed hit effect to one play property

--- scenes/FX/hit_effect.gd
+++ scenes/FX/hit_effect.gd
@@ -4,8 +4,8 @@ extends GPUParticles2D
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	emitting = true # Start particle effect
-	await get_tree().create_timer(lifetime).timeout
-	queue_free() # Destroy after playing
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	if not emitting:
+		queue_free()
