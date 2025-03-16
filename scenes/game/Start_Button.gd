f0e4370ab4e319161fa908d92841cd37e4f300ed
Asim424 <asimhamdan2005@gmail.com>
2025-03-16 16:21:50 -0600

    added hp bar

--- scenes/game/Start_Button.gd
+++ scenes/game/Start_Button.gd
@@ -3,11 +3,10 @@ extends Button
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	var next_scene = preload("res://scenes/game/Game.tscn")
+	#var next_scene = preload("res://scenes/game/Game.tscn")
 	var button = self
 	button.pressed.connect(_button_pressed)
 	add_child(button)
 
 func _button_pressed():
 	get_tree().change_scene_to_file("res://scenes/game/Game.tscn")
-	print("Hello world!")
