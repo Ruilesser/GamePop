2dc5bc2be580231c2a81798c0a77f99e1da6ebd2
Ali Almishri <ali.almishri@ucalgary.ca>
2025-03-15 21:24:19 -0600

    Player sprite and states additions

--- scenes/player/Player.gd
+++ scenes/player/Player.gd
@@ -6,7 +6,7 @@ const BASE_ANIMATION_SPEED: int = 2
 const JUMP_VELOCITY = -400.0
 const DECEL_RATE = 15.0
 
-enum {IDLE, RUN, JUMP, SKID}
+enum {IDLE, RUN, JUMP, SKID, SLIDE}
 
 var movement_state: int = IDLE
 
@@ -14,14 +14,24 @@ func _process(_delta):
 	match movement_state:
 		IDLE:
 			%AnimatedSprite2D.play("idle")
+			set_standing_collision(true);
 		RUN:
 			%AnimatedSprite2D.play("run")
 			%AnimatedSprite2D.speed_scale = BASE_ANIMATION_SPEED
+			set_standing_collision(true);
 		JUMP:
 			%AnimatedSprite2D.play("jump")
+			set_standing_collision(true);
 		SKID:
 			%AnimatedSprite2D.play("idle")
+			set_standing_collision(true);
+		SLIDE:
+			%AnimatedSprite2D.play("slide")
+			set_standing_collision(false)
 
+func set_standing_collision( value ): # toggle between standing and sliding collision
+	$StandingCol.set_disabled(value)
+	$SlidingCol.set_disabled(!value)
 
 func _physics_process(delta):
 
@@ -37,17 +47,20 @@ func _physics_process(delta):
 			
 		# Get the input direction and handle the movement/deceleration.
 		# As good practice, you should replace UI actions with custom gameplay actions.
-		var direction = Input.is_action_pressed("ui_left")
-		if direction:
-			#if direction == 1:
-				#%AnimatedSprite2D.flip_h = false
-			#else:
-				#%AnimatedSprite2D.flip_h = true
-			movement_state = SKID
-			velocity.x = move_toward(velocity.x, 0, DECEL_RATE) #move toward 0 while holding LEFT
-		else:
-			movement_state = RUN
-			velocity.x = SPEED
+		var direction = Input.get_axis("ui_left", "ui_right")
+		if direction:#move in the directionsdadwsa
+			if direction == 1:
+				%AnimatedSprite2D.flip_h = false
+			else:
+				%AnimatedSprite2D.flip_h = true
+			#movement_state = SKID
+			velocity.x = SPEED * direction
+		else:#stop moving
+			if(velocity.x == 0):
+				movement_state = IDLE
+			else:
+				movement_state = SKID
+				velocity.x = move_toward(velocity.x, 0, DECEL_RATE) #move toward 0 while holding LEFT
 		
 	else:
 		movement_state = JUMP
